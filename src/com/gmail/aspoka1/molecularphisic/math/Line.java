b99c26cb48d69218db061a71fef678f4b4ac2138
aspoka <aspoka1@gmail.com>
2020-01-02 11:37:12 +0100

    Added some math

---
+++ src/com/gmail/aspoka1/molecularphisic/math/Line.java
@@ -0,0 +1,68 @@
+package com.gmail.aspoka1.molecularphisic.math;
+
+import java.awt.geom.Point2D;
+
+public class Line {
+    public double a;
+    public double b;
+    public double c;
+
+    public Line(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Line(Point2D.Double p1, Point2D.Double p2) {
+        a = p2.getY() - p1.getY();
+        b = p1.getX() - p2.getX();
+        c = -(a * p1.getX() + b * p1.getY());
+    }
+
+    public Line(Segment s) {
+        this(s.getP1(), s.getP2());
+    }
+
+    public double distance(Point2D.Double p) {
+        return Math.abs(a * p.getX() + b * p.getY() + c) / Math.sqrt(a * a + b * b);
+    }
+
+    public Line parallel(Point2D.Double p) {
+        double c1 = -(a * p.getX() + b * p.getY());
+        return new Line(a, b, c1);
+    }
+
+    public Line perpendicular(Point2D.Double p) {
+        double c1 = a * p.getX() - b * p.getY();
+        return new Line(-a, b, c);
+    }
+
+    public Point2D.Double intersection(Line l) {
+        if((a == l.a) && (b == l.b)) {
+            return null;
+        }
+        double w = a * l.b - l.a * b;
+        double wx = l.c * b - c * l.b;
+        double wy = c * l.a - l.c * a;
+        double x = wx / w;
+        double y = wy / w;
+        return new Point2D.Double(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" + "a=" + a + ", b=" + b + ", c=" + c + '}';
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+}
---
+++ src/com/gmail/aspoka1/molecularphisic/math/Segment.java
@@ -0,0 +1,35 @@
+package com.gmail.aspoka1.molecularphisic.math;
+
+import java.awt.geom.Point2D;
+
+public class Segment {
+    Point2D.Double p1;
+    Point2D.Double p2;
+
+    public Segment(Point2D.Double p1, Point2D.Double p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public Point2D.Double getP1() {
+        return p1;
+    }
+
+    public Point2D.Double getP2() {
+        return p2;
+    }
+
+    public double getLength() {
+        return p1.distance(p2);
+    }
+
+    public double distance(Point2D.Double p) {
+        Line l = new Line(this);
+        Point2D.Double q = l.intersection(l.perpendicular(p));
+        if(((p1.x < p.x) && (p.x < p2.x)) || ((p2.x < p.x) && (p.x < p1.x))) {
+            return l.distance(p);
+        } else {
+            return (p.distance(p1) < p.distance(p2)) ? p.distance(p1) : p.distance(p2);
+        }
+    }
+}
